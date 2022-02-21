7647e6690a6de39b3258fc0b6fb8eedf590597f6
junaid184 <junaidshakil116@gmail.com>
2022-02-21 16:43:25 +0500

    add notes

--- models/Notes.mjs
+++ models/Notes.mjs
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
 const NotesSchema = mongoose.model("Notes", {
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
--- routes/notes.mjs
+++ routes/notes.mjs
@@ -1,12 +1,53 @@
 import express from "express";
+import Notes from "../models/Notes.mjs";
+import fetchuser from "../middleware/fetchuser.mjs";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  obj = {
-    a: "thios",
-    number: 34,
-  };
-  res.json(obj);
+router.get("/fetchallnotes", fetchuser, (req, res) => {
+  try {
+    Notes.find({ email: req.user.email }, (err, notes) => {
+      if (err) {
+        res.status(500).send("error in getting database");
+      } else if (!notes) {
+        return res.status(400).send("there are no notes");
+      } else {
+        res.send(notes);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("internel server error");
+  }
 });
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Enter a valid description").isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    } else {
+      try {
+        Notes.create({
+          title: req.body.title,
+          description: req.body.description,
+          tag: req.body.tag,
+          email: req.user.email,
+          name: req.user.name,
+        })
+          .then((note) => res.send(note))
+          .catch((e) => res.json({ error: "enter a valid note" }));
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("internel server error");
+      }
+    }
+  }
+);
 
 export default router;
